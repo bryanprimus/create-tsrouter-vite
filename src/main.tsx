e544ba7a5f6ba41f99ce76f880b2aeb09214a869
bryanprimus <bryantobing0@gmail.com>
2025-07-04 15:06:38 +0800

    feat: tidy up styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import './index.css'
+import './styles.css'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 

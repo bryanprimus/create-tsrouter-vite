301915d58e6cf11a67fa4c9e6953df5467274f2e
bryanprimus <bryantobing0@gmail.com>
2025-07-04 14:13:50 +0800

    feat: configure prettier with eslint

---
+++ prettier.config.ts
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+import type { Config } from 'prettier'
+
+const config: Config = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+}
+
+export default config
